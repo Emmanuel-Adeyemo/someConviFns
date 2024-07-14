c503907bca2785e08c11e6c83a350af8b622fd1b
Emmanuel Adeyemo <eadeyemo2@gmail.com>
2024-07-13 22:39:55 -0500

    Update placeholder.R

--- placeholder.R
+++ placeholder.R
@@ -3,14 +3,16 @@ xy <- as.data.frame(combn(rownames(tmp), 2))
 yx = sapply(xy, function(x) as.character(x), simplify = FALSE)
 
 
+# pairwise matching
 
-coa_list = list()
+matching_list = list()
+            
 for(pair in 1:length(yx)){
   
   
   tmp_geno = tmp %>% filter(rownames(tmp) %in% yx[[pair]])
   
-  coa_list[[pair]] = get_coa(tmp_geno)
+  matching_list[[pair]] = get_matching(tmp_geno)
 }
 
-a = do.call('rbind', coa_list)
+a = do.call('rbind', matching_list)
